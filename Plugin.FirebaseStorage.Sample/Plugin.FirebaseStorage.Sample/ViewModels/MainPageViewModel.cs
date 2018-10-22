f112a4b7424dcf8ed59bd010649258cd8848ca05
f-miyu <matsushima23@gmail.com>
2018-10-22 15:44:16 +0900

    修正

--- Plugin.FirebaseStorage.Sample/Plugin.FirebaseStorage.Sample/ViewModels/MainPageViewModel.cs
+++ Plugin.FirebaseStorage.Sample/Plugin.FirebaseStorage.Sample/ViewModels/MainPageViewModel.cs
@@ -22,6 +22,10 @@ namespace Plugin.FirebaseStorage.Sample.ViewModels
         public ReactivePropertySlim<ImageSource> Image { get; } = new ReactivePropertySlim<ImageSource>();
         public ReactivePropertySlim<double> UploadProgress { get; } = new ReactivePropertySlim<double>();
         public ReactivePropertySlim<double> DownloadProgress { get; } = new ReactivePropertySlim<double>();
+        public ReactivePropertySlim<string> Name { get; } = new ReactivePropertySlim<string>();
+        public ReactivePropertySlim<long> Size { get; } = new ReactivePropertySlim<long>();
+        public ReactivePropertySlim<DateTime?> CreationTime { get; } = new ReactivePropertySlim<DateTime?>();
+        public ReactivePropertySlim<string> Url { get; } = new ReactivePropertySlim<string>();
 
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
@@ -30,28 +34,33 @@ namespace Plugin.FirebaseStorage.Sample.ViewModels
 
             SelectImageCommand.Subscribe(async () =>
             {
-                if (!CrossMedia.Current.IsPickPhotoSupported)
-                {
-                    return;
-                }
-
-                var file = await CrossMedia.Current.PickPhotoAsync(new Media.Abstractions.PickMediaOptions
-                {
-                    PhotoSize = Media.Abstractions.PhotoSize.Medium
-                });
-
-                if (file == null)
-                    return;
-
                 try
                 {
+                    if (!CrossMedia.Current.IsPickPhotoSupported)
+                    {
+                        return;
+                    }
+
+                    var file = await CrossMedia.Current.PickPhotoAsync(new Media.Abstractions.PickMediaOptions
+                    {
+                        PhotoSize = Media.Abstractions.PhotoSize.Medium
+                    });
+
+                    if (file == null)
+                        return;
+
                     var name = Path.GetFileName(file.Path);
                     var stream = file.GetStream();
 
                     file.Dispose();
 
+                    Image.Value = ImageSource.FromStream(() => null);
                     UploadProgress.Value = 0;
                     DownloadProgress.Value = 0;
+                    Name.Value = null;
+                    Size.Value = 0;
+                    CreationTime.Value = null;
+                    Url.Value = null;
 
                     var reference = CrossFirebaseStorage.Current.Storage.RootReference.GetChild(name);
 
@@ -64,12 +73,12 @@ namespace Plugin.FirebaseStorage.Sample.ViewModels
                         UploadProgress.Value = e.TotalByteCount > 0 ? 100.0 * e.BytesTransferred / e.TotalByteCount : 0;
                     };
 
-                    var metadata = new MetadataChange
+                    var metadataChange = new MetadataChange
                     {
                         ContentType = "image/jpeg"
                     };
 
-                    await reference.PutStreamAsync(stream, metadata, uploadProgress);
+                    await reference.PutStreamAsync(stream, metadataChange, uploadProgress);
                     UploadProgress.Value = 100;
 
                     var downloadProgress = new Progress<IDownloadState>();
@@ -81,10 +90,16 @@ namespace Plugin.FirebaseStorage.Sample.ViewModels
                     var data = await reference.GetStreamAsync(downloadProgress);
                     DownloadProgress.Value = 100;
 
-                    Image.Value = ImageSource.FromStream(() =>
-                    {
-                        return data;
-                    });
+                    Image.Value = ImageSource.FromStream(() => data);
+
+                    var metadata = await reference.GetMetadataAsync();
+
+                    Name.Value = metadata.Name;
+                    Size.Value = metadata.SizeBytes;
+                    CreationTime.Value = metadata.CreationTime?.LocalDateTime;
+
+                    var url = await reference.GetDownloadUrlAsync();
+                    Url.Value = url.ToString();
                 }
                 catch (Exception e)
                 {
